4e91230c7ab13c2e0568b109cc61584ae8b9a771
尹玉慧 <yuhui@yinyuhuideMacBook-Pro.local>
2019-09-24 20:42:55 +0800

    路由优化

---
+++ src/pages/ui/index.js
@@ -0,0 +1,15 @@
+import Buttons from './buttons'
+import Loadings from './loadings'
+import Modals from './modals'
+import Notifications from './notifications'
+import Messages from './messages'
+import Tabs from './tabs'
+
+export {
+    Buttons,
+    Loadings,
+    Modals,
+    Notifications,
+    Messages,
+    Tabs,
+}
--- src/router.js
+++ src/router.js
@@ -5,12 +5,8 @@ import NotMatch from './pages/notMatch'
 import Login from './pages/login'
 import Admin from './Admin'
 import Home from './pages/Home'
-import Buttons from './pages/ui/buttons'
-import Loadings from './pages/ui/loadings'
-import Modals from './pages/ui/modals'
-import Notifications from './pages/ui/notifications'
-import Messages from './pages/ui/messages'
-import Tabs from './pages/ui/tabs'
+import * as UI from './pages/ui'
+
 
 export default class Router extends Component {
     render() {
@@ -22,12 +18,25 @@ export default class Router extends Component {
                         <Admin>
                             <Switch>
                                 <Route path="/admin/home" component={Home} />
+<<<<<<< HEAD
                                 <Route path="/admin/ui/buttons" component={Buttons} />
                                 <Route path="/admin/ui/loadings" component={Loadings} />
                                 <Route path="/admin/ui/modals" component={Modals} />
                                 <Route path="/admin/ui/notifications" component={Notifications} />
                                 <Route path="/admin/ui/messages" component={Messages} />
                                 <Route path="/admin/ui/tabs" component={Tabs} />
+=======
+                                {
+                                    this.getMenuList(UI).map(item => {
+                                        return (
+                                            <Route 
+                                                path={`/admin/ui/${item.lower}`} 
+                                                key={`/admin/ui/${item.lower}`} 
+                                                component={UI[item.upper]} />
+                                        )
+                                    })
+                                }
+>>>>>>> 路由优化
                                 <Route component={NotMatch} />
                             </Switch>
                         </Admin>
@@ -35,6 +44,16 @@ export default class Router extends Component {
                 </App>      
             </HashRouter>
         )
+    }
+
+    getMenuList = (key) => {
+        let menuList = Object.keys(key).map(item => {
+            return {
+                lower: item.toLocaleLowerCase(),
+                upper: item
+            }
+        })
+        return menuList
     }
 }
 
