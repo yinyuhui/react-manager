ffa5bbebca0f8b93be2d96a4f0f7e261acd12d8a
尹玉慧 <yuhui@yinyuhuideMacBook-Pro.local>
2019-09-24 20:54:53 +0800

    解决冲突

--- src/router.js
+++ src/router.js
@@ -18,14 +18,6 @@ export default class Router extends Component {
                         <Admin>
                             <Switch>
                                 <Route path="/admin/home" component={Home} />
-<<<<<<< HEAD
-                                <Route path="/admin/ui/buttons" component={Buttons} />
-                                <Route path="/admin/ui/loadings" component={Loadings} />
-                                <Route path="/admin/ui/modals" component={Modals} />
-                                <Route path="/admin/ui/notifications" component={Notifications} />
-                                <Route path="/admin/ui/messages" component={Messages} />
-                                <Route path="/admin/ui/tabs" component={Tabs} />
-=======
                                 {
                                     this.getMenuList(UI).map(item => {
                                         return (
@@ -36,7 +28,6 @@ export default class Router extends Component {
                                         )
                                     })
                                 }
->>>>>>> 路由优化
                                 <Route component={NotMatch} />
                             </Switch>
                         </Admin>
