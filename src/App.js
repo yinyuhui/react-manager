d53a632a9ed1e553e8e30e7295e52bdd337f03ae
尹玉慧 <yuhui_yin@fpi-inc.com>
2019-12-02 15:37:49 +0800

    使用中文包

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                {this.props.children}
+                <ConfigProvider locale={zhCN}>
+                    {this.props.children}
+                </ConfigProvider>
             </div>
         )
     }
